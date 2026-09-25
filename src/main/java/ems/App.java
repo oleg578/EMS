cfbd7735cde165eefbcbeef1f3ca7a7d6df03ef6
oleg578 <oleg.nagornij@gmail.com>
2026-09-25 10:44:30 +0200

    rebuilt comment to avoid javadoc confusing

--- src/main/java/ems/App.java
+++ src/main/java/ems/App.java
@@ -7,22 +7,16 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Starting Employee Management System...");
-        /**
-         * Loading staff...
-         * */
+        // Loading staff...
         List<Employee> staff = new ArrayList<>(List.of(
                 new Developer("Alice", 5000.575),
                 new Developer("Bob", 4500.00),
                 new Manager("Carol", 7000.00),
                 new Manager("Dave", 6500.00)
         ));
-        /**
-         * "Initializing Employee Management System..."
-         * */
+        // Initializing Employee Management System...
         Ems ems = new Ems();
-        /**
-         * "Running Employee Management System..."
-         */
+        // Running Employee Management System...
         ems.loadStaff(staff).run();
     }
 }
